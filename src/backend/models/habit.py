4b98e54c7f6554e27ac33b44d5ed6b6e1be61941
Massseo <matteo.schnuell@gmail.com>
2026-02-25 08:53:52 +0100

    feat: Added is_valid method to habit

--- src/backend/models/habit.py
+++ src/backend/models/habit.py
@@ -21,3 +21,11 @@ class Habit:
     
     def check_duplicate(self, other_habit: 'Habit') -> bool:
         return self.name == other_habit.name and self.description == other_habit.description and self.frequency == other_habit.frequency
+    
+    def is_valid(self) -> bool:
+        if not self.name or not self.description or not self.frequency:
+            return False
+        if self.difficulty:
+            if self.difficulty < 1 or self.difficulty > 5:
+                return False
+        return True
